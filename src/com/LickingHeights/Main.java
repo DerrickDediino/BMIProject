137cac9746a6b82fce9c73174665ce15b072ee45
Derrick <Derrick.Dedino.2022@student.lhschools.org>
2019-02-14 14:07:31 -0500

    Grammar fix.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -23,13 +23,13 @@ public class Main {
         System.out.println("Hello. What is your name?");
         Name = keyboard.nextLine();
         System.out.println("Greetings " + Name + "." + "  This is the BMI calculator.");
-        System.out.println("Please type your weight in pounds?");
+        System.out.println("Please type your weight in pounds.");
         weightInKg = keyboard.nextInt();
-        System.out.println("Now, do the same for your height(in inches)?");
+        System.out.println("Now, do the same for your height(in inches).");
         heightInM = keyboard.nextInt();
         BMI = weightInKg/(heightInM*heightInM);
         weightInLb = weightInKg*(1/0.453592);
-        heightInInches = heightInM*(0.3048);
+        heightInInches = heightInM*(0.0254);
 
         //Apply
 
