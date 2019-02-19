48de47f320bb860de4ab03e958e93b59646fa893
Derrick <Derrick.Dedino.2022@student.lhschools.org>
2019-02-19 13:46:30 -0500

    changed float to double (everything is now double).

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
        //declare
 
-        int BMI;
-        int weightInKg, heightInM;
+        double BMI;
+        double weightInKg, heightInM;
         double weightInLb, heightInInches;
         String Name;
         Scanner keyboard;
@@ -24,11 +24,11 @@ public class Main {
         Name = keyboard.nextLine();
         System.out.println("Greetings " + Name + "." + "  This is the BMI calculator.");
         System.out.println("Please type your weight in pounds.");
-        weightInKg = keyboard.nextInt();
+        weightInLb = keyboard.nextDouble();
         System.out.println("Now, do the same for your height(in inches).");
-        heightInM = keyboard.nextInt();
-        weightInLb = weightInKg*(0.453592);
-        heightInInches = heightInM*(0.0254);
+        heightInInches = keyboard.nextDouble();
+        weightInKg = weightInLb*(0.453592);
+        heightInM = heightInInches*(0.0254);
         BMI = weightInKg/(heightInM*heightInM);
 
 
